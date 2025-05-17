5fb83dd51a21f755b970e68ef24dd6e380a26032
roninlp <ronin.lp@gmail.com>
2025-05-18 00:22:48 +0330

    feat: enhance HabitsPage with dehydrate state for improved data hydration

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,6 +1,5 @@
 import { getQueryClient, trpc } from "@/trpc/server";
-import { HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import CreateHabit from "./_components/create-habit";
 import HabitsList from "./_components/habits-list";
 
@@ -9,12 +8,10 @@ export default async function HabitsPage() {
 	void queryClient.prefetchQuery(trpc.habit.getAll.queryOptions());
 
 	return (
-		<HydrationBoundary>
+		<HydrationBoundary state={dehydrate(queryClient)}>
 			<div className="flex justify-between gap-4">
 				<CreateHabit />
-				<Suspense fallback={<div>Loading...</div>}>
-					<HabitsList />
-				</Suspense>
+				<HabitsList />
 			</div>
 		</HydrationBoundary>
 	);
