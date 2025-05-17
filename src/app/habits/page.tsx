ad82bb5579c9a7ca933ec8fdf37424f2924d248e
roninlp <ronin.lp@gmail.com>
2025-05-18 00:11:03 +0330

    feat: wrap HabitsPage component with HydrateClient for improved data handling

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -1,4 +1,4 @@
-import { api } from "@/trpc/server";
+import { HydrateClient, api } from "@/trpc/server";
 import { Suspense } from "react";
 import CreateHabit from "./_components/create-habit";
 import HabitsList from "./_components/habits-list";
@@ -7,11 +7,13 @@ export default async function HabitsPage() {
 	void api.habit.getAll.prefetch();
 
 	return (
-		<div className="flex justify-between gap-4">
-			<CreateHabit />
-			<Suspense fallback={<div>Loading...</div>}>
-				<HabitsList />
-			</Suspense>
-		</div>
+		<HydrateClient>
+			<div className="flex justify-between gap-4">
+				<CreateHabit />
+				<Suspense fallback={<div>Loading...</div>}>
+					<HabitsList />
+				</Suspense>
+			</div>
+		</HydrateClient>
 	);
 }
